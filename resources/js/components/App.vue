2fe70eaa26ad87468a686eb500ca3992f8e2231b
emoshawty666 <homixidehmxgang@gmail.com>
2025-04-04 15:52:57 +0300

    lastdance

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="article != null" class="alert alert-primary" role="alert">
-        Добавлена новая статья <strong> <a :href="`/article/${article.id}`"> {{ article.name }}</a></strong>
+        Добавлена новая статья <strong> <a :href="'/article' + article.id"> {{ article.title }}</a></strong>
     </div>
 </template>
 
@@ -8,10 +8,10 @@
     export default {
     data() { return { article: null } },
         created() {
-            window.Echo.channel('test').listen('NewArticleEvent', (article) => {
+            window.Echo.channel('test').listen('ArticleCreateEvent', (article) => {
                 console.log(article);
                 this.article=article.article;
-                alert('Добавлена новая статья!');
+                // alert('Добавлена новая статья!');
             })
         }
     }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bootstrap Site</title>
@@ -52,6 +53,10 @@
       </div>
   </div>
 </nav>
+<div id="app">
+
+ </div>
+
     </header>
     <main>
         <div class="container mt-3">
